623e0f9656525c07d6929ccb939958730bf51d13
Liz-est <xinlupan35@gamil.com>
2026-04-03 14:07:29 +0800

    new modification

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    print("HAHA")
+    print("HIHI")
+    return a + b
+if __name__ == '__main__':
+    result = add(2, 3)
+def multiply(a, b):
+    print("MULTIPLY")
+    return a * b
+print(multiply(2, 3))
