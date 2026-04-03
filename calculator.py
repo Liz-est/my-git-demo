1cd3570624452eb5c72e1142a33ed7c55f962a5a
Liz-est <xinlupan35@gamil.com>
2026-04-03 14:22:42 +0800

    try

--- calculator.py
+++ calculator.py
@@ -1,3 +1,14 @@
 def add(a,b):
     return a + b
 print(add(2,3))
+def subtract(a,b):
+    return a - b
+print(subtract(2,3))
+def multiply(a,b):
+    return a * b
+print(multiply(2,3))
+def divide(a,b):
+    return a / b
+print(divide(2,3))
+
+#why is this here?
